ad9d2201521673c25b0acb18864e2af93ce7e200
maleesha6850 <maleeshamp942@gmail.com>
2025-12-30 12:58:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=HopzVLDS#jErZPJTchxox1cGiM5BBuR7YhRDiEIRpR2vCCh90m4w', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
+SESSION_ID: process.env.SESSION_ID || 'VISPER-MD&51102afe-e5c1-47bc-889c-753014621b9b', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
 PORT: process.env.PORT || '3000',
 MONGODB: process.env.MONGODB || 'put_your_mongodb_url' // අනිවාරෙන් Mongodb Url එකක් ඕනි දන්නෙ නැත්නම් මගෙ වීඩියෝ එක බලලා හදාගන්න.🔥
 };
